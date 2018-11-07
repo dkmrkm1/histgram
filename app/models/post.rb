5a8af3f1fa6f225d0740d6009134f690eb2aef58
Duuun <daimura10@gmail.com>
2018-11-07 15:09:25 +0900

    Added form to upload images

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
   validates :content, {presence: true}
+  mount_uploader :image, ImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_06_054147) do
+ActiveRecord::Schema.define(version: 2018_11_07_054813) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
 end
