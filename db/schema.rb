68a507c53f3aab3cd937c54ead25f5257a6ca835
Duuun <daimura10@gmail.com>
2018-11-18 01:53:56 +0900

    Added User and user's validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_16_123657) do
+ActiveRecord::Schema.define(version: 2018_11_17_165024) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2018_11_16_123657) do
     t.string "image"
     t.string "slug"
     t.index ["slug"], name: "index_posts_on_slug", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
