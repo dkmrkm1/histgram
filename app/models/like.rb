8279a32b5f943752efb67f643e98fe8fcfe5f9bd
Duuun <daimura10@gmail.com>
2019-06-08 18:35:15 +0900

    add annotate models

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: likes
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  post_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Like < ApplicationRecord
   validates :user_id, {presence: true}
   validates :post_id, {presence: true}
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  email      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  password   :string
+#  image      :string
+#  content    :string
+#
+
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
